7fc8035edd00c611795170f6ce4e59bdfc3418a0
Lucas Morais <lucascdemorais@gmail.com>
2022-08-16 12:05:12 -0300

    adjusting

--- initial.sh
+++ initial.sh
@@ -23,6 +23,9 @@ apt install nodejs
 apt install npm
 apt install zsh
 
+git config --global user.name "Lucas Morais"
+git config --global user.email "lucascdemorais@gmail.com"
+
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
