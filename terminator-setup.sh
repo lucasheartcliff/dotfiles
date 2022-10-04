9b30e89bb54c85699f34587c443a6e5cd4f0270e
Lucas Morais <lucascdemorais@gmail.com>
2022-10-04 18:57:14 -0300

    adding os verification on terminator

--- terminator-setup.sh
+++ terminator-setup.sh
@@ -1,8 +1,13 @@
-#!bin/bash
+source ./check_os.sh
 
-sudo add-apt-repository ppa:gnome-terminator
-sudo apt-get install terminator
-sudo update-alternatives --config x-terminal-emulator
+if [ is_fedora ]; then
+    sudo dnf -y makecache --refresh
+    sudo dnf -y install terminator
+else
+    sudo add-apt-repository ppa:gnome-terminator
+    sudo apt-get install terminator
+    sudo update-alternatives --config x-terminal-emulator
+fi
 
 mkdir $HOME/.config/terminator/
 cp ./configs/terminator/terminator-config $HOME/.config/terminator/config
