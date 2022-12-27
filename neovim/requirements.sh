af62400e2a87b5885514feae3f28d368e1eccdcd
Lucas Morais <lucascdemorais@gmail.com>
2022-12-27 16:30:26 -0300

    adjusting project structure

--- neovim/requirements.sh
+++ neovim/requirements.sh
@@ -4,11 +4,3 @@ nvm install 16
 nvm use 16
 
 yarn global add typescript-language-server @fsouza/prettierd
-
-BASE=$(pwd)
-
-rm -rf $HOME/.config/nvim/
-mkdir $HOME/.config/nvim
-
-cp -r ./configs/neovim/* $HOME/.config/nvim
-
---
+++ neovim/sync-config.sh
@@ -0,0 +1,8 @@
+
+rm -rf $HOME/.config/nvim/lua
+rm -f $HOME/.config/nvim/init.lua
+
+mkdir $HOME/.config/nvim
+
+cp -r ./configs/* $HOME/.config/nvim
+
