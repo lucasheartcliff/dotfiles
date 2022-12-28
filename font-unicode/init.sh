91d2b7d90a3dd615ae09d22a1cac7d1c4a93e6bb
Lucas Morais <lucascdemorais@gmail.com>
2022-12-28 17:22:24 -0300

    adding fonts setup

---
+++ font-unicode/init.sh
@@ -0,0 +1,21 @@
+source ./check_os.sh
+if [ is_fedora ]; then
+    sudo dnf makecache --refresh
+    sudo dnf -y install fira-code-fonts
+
+else
+    sudo apt install fonts-firacode
+fi
+
+FOLDER_PATH="/tmp/$(uuidgen -r)/"
+
+mkdir $FOLDER_PATH
+
+git clone https://github.com/terroo/fonts.git $FOLDER_PATH
+
+mkdir $HOME/.local/share/fonts/
+cd $FOLDER_PATH/fonts && cp *.ttf *.otf $HOME/.local/share/fonts/
+
+fc-cache -fv
+
+rm -rf $FOLDER_PATH
