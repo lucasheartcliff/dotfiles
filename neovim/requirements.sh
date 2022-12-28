470f518c4a694ebaebbd72475045bf073936ad2c
Lucas Morais <lucascdemorais@gmail.com>
2022-12-28 17:13:59 -0300

    adding setup for ubuntu on neovim

--- neovim/requirements.sh
+++ neovim/requirements.sh
@@ -1,6 +1,12 @@
-sudo dnf install -y neovim python3-neovim gcc-c++
+source ../check_os.sh
+
+if [ is_fedora ]; then
+  sudo dnf install -y neovim python3-neovim gcc-c++
+else
+  sudo apt install -y neovim python3-neovim gcc-c++
+fi;
 
 nvm install 16
 nvm use 16
 
-yarn global add typescript-language-server @fsouza/prettierd
+yarn global add typescript-language-server
