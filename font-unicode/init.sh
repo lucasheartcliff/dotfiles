70aab7b05921a9de62a1ce026fe784058cb5ab9c
Lucas Morais <lucascdemorais@gmail.com>
2023-01-02 18:13:18 -0300

    adding setup for lunarvim

--- font-unicode/init.sh
+++ font-unicode/init.sh
@@ -14,6 +14,12 @@ mkdir $FOLDER_PATH
 git clone https://github.com/terroo/fonts.git $FOLDER_PATH
 
 mkdir $HOME/.local/share/fonts/
+
+echo "Copying fonts in folder"
+cp ./nerd-fonts/**/*.ttf  $HOME/.local/share/fonts/
+
+
+echo "Copying nerd-fonts"
 cd $FOLDER_PATH/fonts && cp *.ttf *.otf $HOME/.local/share/fonts/
 
 fc-cache -fv
