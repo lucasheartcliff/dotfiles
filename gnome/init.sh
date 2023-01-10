8ad22caf7626a68702afad3d10d223d243982470
Lucas Morais <lucascdemorais@gmail.com>
2023-01-09 23:55:19 -0300

    adding customizations for gnome

---
+++ gnome/init.sh
@@ -0,0 +1,10 @@
+DIR_PATH="/tmp/$(uuidgen -r)/"
+
+git clone git@github.com:vinceliuice/Orchis-theme.git $DIR_PATH
+$DIR_PATH/install.sh -l
+rm -rf $DIR_PATH
+
+git clone git@github.com:TaylanTatli/Sevi.git $DIR_PATH
+$DIR_PATH/install.sh -a
+rm -rf $DIR_PATH
+
