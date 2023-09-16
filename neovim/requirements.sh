2cbddbe311d25bdc2f84f85156bb538fd4d1921e
Lucas Morais <lucascdemorais@gmail.com>
2023-09-16 00:37:46 -0300

    adding ignore folders

--- neovim/requirements.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source ../check_os.sh
-
-if [ is_fedora ]; then
-  sudo dnf install -y neovim python3-neovim gcc-c++
-else
-  sudo apt install -y neovim python3-neovim;
-  sudo apt-get install python-dev python-pip python3-dev python3-pip;
-fi;
