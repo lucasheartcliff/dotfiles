002a976d86e78b6f6217c859c87a8aef1fbaa31a
Lucas Morais <lucascdemorais@gmail.com>
2023-09-20 15:33:50 -0300

    adding xclip

--- fedora.sh
+++ fedora.sh
@@ -8,6 +8,6 @@ sudo dnf update
 
 printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 
-sudo dnf install -y brave-browser brave-keyring terminator tmux git curl wget make code stow xorg-x11-utils
+sudo dnf install -y brave-browser brave-keyring terminator tmux git curl wget make code stow xorg-x11-utils xclip
 
 sh ./tools-exec.sh
