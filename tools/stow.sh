b2f3b1a62312ec8642d87ba97922229884ef8dfd
Lucas Morais <lucascdemorais@gmail.com>
2023-09-20 11:48:54 -0300

    fixing commands

--- tools/stow.sh
+++ tools/stow.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 STOW=$HOME/.stow
+CURR_PATH=$(pwd)
+rm -rf $STOW
 git clone https://github.com/aspiers/stow.git $STOW
 
-bash $STOW/bootstrap
-bash $STOW/configure
-make $STOW/
-sudo make install $STOW
+cd $STOW
+
+bash bootstrap
+bash configure
+make 
+sudo make install ./
 stow --version
+
+cd $CURR_PATH
