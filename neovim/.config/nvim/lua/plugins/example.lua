d18413a8b0e9d0c905e2d99612695be73529b04d
Lucas Morais <lucascdemorais@gmail.com>
2023-09-27 20:58:59 -0300

    start to switch to lazyvim bootstrap

---
+++ neovim/.config/nvim/lua/plugins/example.lua
@@ -0,0 +1,49 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+if true then return {} end
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+
+
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+
+  -- disable trouble
+  { "folke/trouble.nvim",      enabled = false },
+
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+
+  -- change some telescope options and a keymap to browse plugin files
+  
+  
+
+  
+  -- use mini.starter instead of alpha
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+
+}
---
+++ neovim/.config/nvim/lua/plugins/wakatime.lua
@@ -0,0 +1,5 @@
+M ={
+-- Wakatime
+  { 'wakatime/vim-wakatime' }
+}
+return M
