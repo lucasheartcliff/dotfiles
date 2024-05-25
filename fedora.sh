cc562c6e5f1abeb19a11ffb739e213f46cbb848a
Lucas Morais <lucascdemorais@gmail.com>
2024-05-24 21:54:38 -0300

    adding alacritty setup

--- fedora.sh
+++ fedora.sh
@@ -9,6 +9,10 @@ sudo dnf update
 
 printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 
-sudo dnf install -y brave-browser brave-keyring terminator tmux git curl wget make code stow zsh xclip ripgrep
+sudo dnf install -y brave-browser \
+	brave-keyring terminator tmux git curl wget make code cmake freetype-devel \
+	fontconfig-devel libxcb-devel g++ clang stow zsh xclip ripgrep
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 sh ./tools_install.sh
--- tools_install.sh
+++ tools_install.sh
@@ -5,20 +5,21 @@ directory="tools/"
 
 # Check if the directory exists
 if [ ! -d "$directory" ]; then
-  echo "The directory '$directory' does not exist."
-  exit 1
+	echo "The directory '$directory' does not exist."
+	exit 1
 fi
 
 # Loop to iterate through all .sh files in the directory
 for script in "$directory"*.sh; do
-  if [ -f "$script" ] && [ -x "$script" ]; then
-    echo "Executing script: $script"
-    bash "$script"   # Execute the script
-  else
-    echo "Unable to execute script: $script"
-  fi
+	if [ -f "$script" ] && [ -x "$script" ]; then
+		echo "Executing script: $script"
+		bash "$script" # Execute the script
+	else
+		echo "Unable to execute script: $script"
+	fi
 done
 
 bash font-unicode/init.sh
+bash alacritty/init.sh
 
 echo "All scripts have been executed."
