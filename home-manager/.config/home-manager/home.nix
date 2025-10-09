6e027cc4dc87a62ecef028a43a5ce4cbc4855d08
Lucas Morais <lucas.morais@intelie.com>
2025-10-09 18:45:15 -0300

    Replace Alacritty with Kitty in Home Manager configuration and add commented settings for Hyprland and GTK themes

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -40,7 +40,7 @@
     pkgs.vscode
     pkgs.tmux
     pkgs.lsd
-    pkgs.alacritty
+    pkgs.kitty
     pkgs.xclip
     pkgs.gnumake
     pkgs.cmatrix
@@ -102,4 +102,57 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  # programs.kitty.enable = true; # required for the default Hyprland config
+  # wayland.windowManager.hyprland.enable = true; # enable Hyprland
+  #
+  # # Optional, hint Electron apps to use Wayland:
+  # # home.sessionVariables.NIXOS_OZONE_WL = "1";
+  # #
+  #  wayland.windowManager.hyprland.settings = {
+  #   "$mod" = "SUPER";
+  #   bind =
+  #     [
+  #       "$mod, F, exec, firefox"
+  #       ", Print, exec, grimblast copy area"
+  #     ]
+  #     ++ (
+  #       # workspaces
+  #       # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+  #       builtins.concatLists (builtins.genList (i:
+  #           let ws = i + 1;
+  #           in [
+  #             "$mod, code:1${toString i}, workspace, ${toString ws}"
+  #             "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+  #           ]
+  #         )
+  #         9)
+  #     );
+  # };
+  # wayland.windowManager.hyprland.systemd.variables = ["--all"];
+  # home.pointerCursor = {
+  #   gtk.enable = true;
+  #   # x11.enable = true;
+  #   package = pkgs.bibata-cursors;
+  #   name = "Bibata-Modern-Classic";
+  #   size = 16;
+  # };
+  #
+  # gtk = {
+  #   enable = true;
+  #
+  #   theme = {
+  #     package = pkgs.flat-remix-gtk;
+  #     name = "Flat-Remix-GTK-Grey-Darkest";
+  #   };
+  #
+  #   iconTheme = {
+  #     package = pkgs.adwaita-icon-theme;
+  #     name = "Adwaita";
+  #   };
+  #
+  #   font = {
+  #     name = "Sans";
+  #     size = 11;
+  #   };
+  # };
 }
